91835545eb9cf99e59e5257fc3aed570326518e7
Grant Oliveira <grant.oliveira@gmail.com>
2019-09-23 14:24:52 -0700

    Created a new function to explore itineraries from one city to another by sampling prices for trips of a given duration on some interval. Sample usage included.

--- Skiplagged Scraper.R
+++ Skiplagged Scraper.R
@@ -1,56 +1,104 @@
 library(rvest)
 library(RSelenium)
 library(tidyverse)
+library(lubridate)
+
 
 airport_codes <- read_csv("airport-codes.csv")
 base_url <- "https://skiplagged.com/flights"
 
+init <- function(){
+  driver <- rsDriver(port = 4774L, browser = c("chrome"), chromever="76.0.3809.25")
+  
+  remDr <- driver[["client"]]
+  
+}
 
-create_flight_url <- function(from, to, leave, return){
+
+
+create_flight_url <- function(from, to, leave = NULL, return = NULL){
   
-  from_airport <- airport_codes[which(airport_codes$municipality == from),]
   
-  if(nrow(from_airport > 1)){
+  from_airport <- airport_codes %>% 
+    filter(type %in% c("large_airport", "medium_airport")) %>% 
+    filter(municipality == from)
+  
+  
+  if(nrow(from_airport) == 0){
+  
+    from_airport <- airport_codes %>% 
+      filter(!type %in% c("large_airport", "medium_airport"))  %>% 
+      filter(municipality == from)
+    
+    if(nrow(from_airport) == 0){
+      message("No airports found")
+    }
+    
+  } else if(nrow(from_airport) > 1){
     message("Multiple Airports in Departure City.")
-    from_options <- from_airport %>% select(type, iata_code) %>% distinct()
+    
+    from_options <- from_airport %>% 
+      select(type, iata_code) %>% distinct()
+    
     print(from_options)
     departure_prompt <- readline(prompt =  "Which one?")
     from_airport <- from_options$iata_code[as.numeric(departure_prompt)]
+  } else if(nrow(from_airport) == 1){
+    
+  from_airport <- from_airport$iata_code[1]
     
   }
+
+
+ # to = "Fresno"
+to_airport <- airport_codes %>% 
+    filter(type %in% c("large_airport", "medium_airport")) %>% 
+    filter(municipality == to)
   
   
-  to_airport <- airport_codes[which(airport_codes$municipality == to),]
-  
-  if(nrow(to_airport) > 1){
-    message("Multiple Airports in Arrival City.")
-    to_options <- to_airport %>% select(type, iata_code) %>% distinct()
+  if(nrow(to_airport) == 0){
+    
+    to_airport <- airport_codes %>% 
+      filter(!type %in% c("large_airport", "medium_airport"))  %>% 
+      filter(municipality == to)
+    
+    if(nrow(to_airport) == 0){
+      message("No airports found")
+    }
+    
+  } else if(nrow(to_airport) > 1){
+    message("Multiple Airports in Departure City.")
+    
+    to_options <- to_airport %>% 
+      select(type, iata_code) %>% distinct()
+    
     print(to_options)
-    arrival_prompt <- readline(prompt =  "Which one?")
-    to_airport <- to_options$iata_code[as.numeric(arrival_prompt)]
+    departure_prompt <- readline(prompt =  "Which one?")
+    to_airport <- to_options$iata_code[as.numeric(departure_prompt)]
+    
+  } else if(nrow(to_airport) == 1){
+    
+    to_airport <- to_airport$iata_code[1]
     
   }
   
-  flight_url <- paste(base_url, from_airport, to_airport, leave, return, sep="/")
+  
+  if(is.null(leave) | is.null(return)){
+  flight_url <- paste(base_url, from_airport, to_airport, sep="/")
+  } else{
+    flight_url <- paste(base_url, from_airport, to_airport, leave, return, sep="/")
+    
+  }
+
   return(flight_url)
   
   
 }
 
 
-driver <- rsDriver(port = 4774L, browser = c("chrome"), chromever="76.0.3809.25")
+extract_elements <- function(x, using = 'xpath'){
 
-remDr <- driver[["client"]]
-
-remDr$navigate(skiplagged_url)
-
-
-trip_xpath <- '//*[contains(concat( " ", @class, " " ), concat( " ", "no-touch", " " ))]'
-
-
-extract_elements <- function(x){
-
-  elements <- remDr$findElements(using = 'xpath', x)
+  elements <- remDr$findElements(using = using, x)
 
   extract_trip_from_element <- function(x){
     trip_data <- x$getElementText()
@@ -66,7 +114,9 @@ return(data)
 
 
 
-raw_list_to_dataframe <- function(x){
+
+
+raw_list_to_dataframe <- function(x, leave, return){
   
   clear_blanks <- function(x){
     if(length(x) < 3){
@@ -99,6 +149,8 @@ raw_list_to_dataframe <- function(x){
   }
      
 clean_data_frame <- clean_data_frame[complete.cases(clean_data_frame),] 
+clean_data_frame$leave <- leave
+clean_data_frame$return <- return
   
 return(clean_data_frame)
 
@@ -109,19 +161,82 @@ return(clean_data_frame)
 
 
 
+explore_prices <- function(from, to, leave = NULL, return = NULL, duration, interval = NULL){
 
-skiplagged_url <- create_flight_url(from = "Seattle", 
-                                    to = "Fresno",
-                                    leave = "2019-09-27", 
-                                    return = "2019-09-30")
+if(is.null(leave) & !is.null(duration)){  
+  
+
+message("Creating date sequence")
+
+end_date <- Sys.Date() + days(30) 
+
+if(is.null(interval)){
+  interval <- duration
+  message("Defaulting to Trip Duration as Interval")
+}
+
+
+start <- seq.Date(from=Sys.Date(), to = end_date, by = interval)
+end <- start + days(duration)
+
+dates <- tibble(start, end)
+dates$start <- as.character(dates$start)
+dates$end <- as.character(dates$end)
+
+skiplagged_url <- create_flight_url(from = from, 
+                                    to = to)
+
+
+
+all_dates <- tibble()
+
+for(row in 1:nrow(dates)){
+
+path <- paste0("/", dates$start[row], "/", dates$end[row])
+
+new_url <- paste0(skiplagged_url, path)
+
+
+remDr$navigate(new_url)
+
+
+Sys.sleep(1.5)
+
+trip_xpath <- '//*[contains(concat( " ", @class, " " ), concat( " ", "no-touch", " " ))]'
+
 
 
 data <- extract_elements(trip_xpath)
 
-clean_data <- data %>% raw_list_to_dataframe()
+clean_data <- data %>% raw_list_to_dataframe(leave = dates$start[row], 
+                                             return = dates$end[row])
+
+all_dates <- bind_rows(all_dates, clean_data)
+
+}
+
+all_dates$price <- as.numeric(gsub("\\$", "", all_dates$price))
+all_dates <- all_dates %>% arrange(price)
+return(all_dates)
+}
+}
 
 
 
+options <- explore_prices("Los Angeles", "Seattle", interval = 2, duration = 3)
+
+options$price <- as.numeric(gsub("\\$", "", options$price))
+
+options %>% group_by(leave) %>% 
+  summarize(price = min(price)) %>% 
+  ggplot(aes(x=as.Date(leave), y = price)) +
+  geom_line() + scale_y_continuous(labels = scales::dollar) + scale_x_date(date_labels = "%a %B %d") +
+  labs(x="Date", y = "Price") + ggtitle("3 Day Flights LAX-Seattle")
+
+
+# airlines_xpath <- '//*[contains(concat( " ", @class, " " ), concat( " ", "airlines-lg", " " ))]'
+
+# extract_elements('//*span//[contains(concat( " ", @class, " " ), concat( " ", "airlines-lg", " " ))]', "xpath")
 
 
 
